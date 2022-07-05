98113cf7a4be02d75089506ee87ffbc2dd5a3271
beeavelar <avelar.dbr@gmail.com>
2022-07-05 14:33:18 -0300

    CRUD Owners

---
+++ MyLeasing.Web/MyLeasing.Web/Data/Entities/Owner.cs
@@ -0,0 +1,20 @@
+﻿namespace MyLeasing.Web.Data.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        public int Document { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int FixedPhone { get; set; }
+
+        public int CellPhone { get; set; }
+
+        public string Addrress { get; set; }
+
+    }
+}
